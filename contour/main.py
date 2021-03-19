578d309908a55c1ed4e2d4e8c85dce113200f47b
孙啸寒 <sunxiaohan01@corp.netease.com>
2021-03-19 12:06:32 +0800

    core function

--- contour/main.py
+++ contour/main.py
@@ -17,7 +17,15 @@ class QuadDetector(object):
 
         self.VLINE1, self.VLINE2, self.HLINE1, self.HLINE2 = self.initFixedLines()
         self.vLines, self.hLines = self.initQuadLines()
-        self.getFourLines()
+        self.topLeft, self.bottomLeft, self.bottomRight, self.topRight = self.getQuadCorners()
+
+    def getCoordinates(self):
+        # inverse clock
+        coordinates = [str(self.topLeft.x), str(self.topLeft.y),
+                       str(self.bottomLeft.x), str(self.bottomLeft.y),
+                       str(self.bottomRight.x), str(self.bottomRight.y),
+                       str(self.topRight.x), str(self.topRight.y)]
+        return coordinates
 
     def initFixedLines(self):
         VLINE1 = Line(Point(0, 0), Point(0, self.height))
@@ -30,14 +38,15 @@ class QuadDetector(object):
         # process image
         gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-        black = gray.copy()
-        black[:, :] = np.zeros(1, dtype=np.uint8)
+        contourImg = gray.copy()
+        contourImg[:, :] = np.zeros(1, dtype=np.uint8)
         contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         contours = sorted(contours, key=cv2.contourArea, reverse=True)[1:4]
-        cv2.drawContours(black, contours, -1, 255, 3)
-        cannyEdges = cv2.Canny(black, 50, 150)
+        cv2.drawContours(contourImg, contours, -1, 255, 3)
+        cannyImg = cv2.Canny(contourImg, 50, 150)
         # group vertical and horizontal lines
-        lines = cv2.HoughLinesP(cannyEdges, 1, np.pi / 180, 100, maxLineGap=200)
+        lines = cv2.HoughLinesP(cannyImg, 1, np.pi / 180, 100, minLineLength=300, maxLineGap=100)
+        print(f'HoughLinesP number: {len(lines)}')
         vLines = []
         hLines = []
         for line in lines:
@@ -49,21 +58,41 @@ class QuadDetector(object):
                 hLines.append(line)
             else:
                 vLines.append(line)
-
         if self.show:
-            cv2.imshow('contours', black)
-            cv2.imshow('canny edges', cannyEdges)
+            cv2.imshow('contours', contourImg)
+            cv2.imshow('canny edges', cannyImg)
         return vLines, hLines
 
-    def getFourLines(self):
+    def getQuadCorners(self):
         top1, bottom1 = self.intersectVLINE(self.VLINE1)
         top2, bottom2 = self.intersectVLINE(self.VLINE2)
+        left1, right1 = self.intersectHLINE(self.HLINE1)
+        left2, right2 = self.intersectHLINE(self.HLINE2)
+
+        topLine = Line(top1, top2)
+        bottomLine = Line(bottom1, bottom2)
+        leftLine = Line(left1, left2)
+        rightLine = Line(right1, right2)
+        topLeft = GetCrossPoint(topLine, leftLine)
+        topRight = GetCrossPoint(topLine, rightLine)
+        bottomLeft = GetCrossPoint(bottomLine, leftLine)
+        bottomRight = GetCrossPoint(bottomLine, rightLine)
 
         if self.show:
             self.drawPoint(top1, color=(0, 0, 255), thickness=-1)
             self.drawPoint(top2, color=(0, 0, 255), thickness=-1)
             self.drawPoint(bottom1, color=(0, 0, 255), thickness=-1)
             self.drawPoint(bottom2, color=(0, 0, 255), thickness=-1)
+            self.drawLine(topLine, color=(0, 0, 255))
+            self.drawLine(bottomLine, color=(0, 0, 255))
+            self.drawLine(leftLine, color=(0, 0, 255))
+            self.drawLine(rightLine, color=(0, 0, 255))
+            self.drawPoint(topLeft, color=(0, 0, 255), thickness=-1, size=5)
+            self.drawPoint(topRight, color=(0, 0, 255), thickness=-1, size=5)
+            self.drawPoint(bottomLeft, color=(0, 0, 255), thickness=-1, size=5)
+            self.drawPoint(bottomRight, color=(0, 0, 255), thickness=-1, size=5)
+
+        return topLeft, bottomLeft, bottomRight, topRight
 
     def intersectVLINE(self, VLINE):
         # TODO: use hLines to intersect with VLINE
@@ -77,18 +106,36 @@ class QuadDetector(object):
         kMeans = KMeans(n_clusters=2, random_state=0).fit(crossPoints)
         topPoint = Point(kMeans.cluster_centers_[0][0], kMeans.cluster_centers_[0][1])
         bottomPoint = Point(kMeans.cluster_centers_[1][0], kMeans.cluster_centers_[1][1])
+        if topPoint.y > bottomPoint.y:
+            temp = topPoint
+            topPoint = bottomPoint
+            bottomPoint = temp
         return topPoint, bottomPoint
 
     def intersectHLINE(self, HLINE):
         # TODO: use vLines to intersect with HLINE
-        pass
+        crossPoints = []
+        for line in self.vLines:
+            point = GetCrossPoint(line, HLINE)
+            if self.show:
+                self.drawPoint(point, color=(0, 255, 0))
+            crossPoints.append([point.x, point.y])
+        crossPoints = np.array(crossPoints)
+        kMeans = KMeans(n_clusters=2, random_state=0).fit(crossPoints)
+        leftPoint = Point(kMeans.cluster_centers_[0][0], kMeans.cluster_centers_[0][1])
+        rightPoint = Point(kMeans.cluster_centers_[1][0], kMeans.cluster_centers_[1][1])
+        if leftPoint.x > rightPoint.x:
+            temp = leftPoint
+            leftPoint = rightPoint
+            rightPoint = temp
+        return leftPoint, rightPoint
 
-    def drawLine(self, line):
-        cv2.line(self.img, (int(line.p1.x), int(line.p1.y)), (int(line.p2.x), int(line.p2.y)), (0, 255, 0), 1)
+    def drawLine(self, line, color=(0, 255, 0)):
+        cv2.line(self.img, (int(line.p1.x), int(line.p1.y)), (int(line.p2.x), int(line.p2.y)), color, 1)
         return
 
-    def drawPoint(self, point, color=(0, 0, 255), thickness=1):
-        cv2.circle(self.img, (int(point.x), int(point.y)), 3, color, thickness=thickness)
+    def drawPoint(self, point, color=(0, 0, 255), thickness=1, size=3):
+        cv2.circle(self.img, (int(point.x), int(point.y)), size, color, thickness=thickness)
         return
 
     def hang(self):
@@ -103,63 +150,16 @@ class QuadDetector(object):
 
 
 def main():
-    img = cv2.imread("./butterfly.png")
-    img = cv2.imread("./bf.png")
+    img = cv2.imread('assets/scan1.jpg')
+    # img = cv2.imread('assets/butterfly.png')
+    # img = cv2.imread('assets/scan4.jpg')
 
-    # -------------------------------
     quadDetector = QuadDetector(img)
-
+    coordinates = quadDetector.getCoordinates()
     quadDetector.hang()
-    return
-    # -------------------------------
-    height, width = img.shape[:2]
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    ret, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # get the [1, 4] largest area contours
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[1:2]
-    # cv2.drawContours(img, contours, -1, (0,0,255), 3)
-
-    # get the edges from contours
-    black = gray.copy()
-    black[:, :] = np.zeros(1, dtype=np.uint8)
-    cv2.drawContours(black, contours, -1, 255, 3)
-    edges = cv2.Canny(black, 50, 150)
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, maxLineGap=200)
-    print(f'lines number: {len(lines)}')
-
-    VLINE = Line(Point(0, 0), Point(0, height))
-    HLINE = Line(Point(0, 0), Point(width, 0))
-    vLines = []
-    hLines = []
-
-    for i in range(len(lines)):
-        x1, y1, x2, y2 = lines[i][0]
-        grad = (y2 - y1) / (x2 - x1)
-        line = Line(Point(x1, y1), Point(x2, y2))
-        line.grad = grad
-        if np.abs(grad) < 1:
-            hLines.append(line)
-        else:
-            vLines.append(line)
-
-    cps = []
-    for line in hLines:
-        cp = GetCrossPoint(line, VLINE)
-        cps.append([cp.x, cp.y])
-        print(f"Cross point: {(cp.x, cp.y)}")
-    cps = np.array(cps)
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(cps)
-    print(f'cross points: {cps} label: {kmeans.labels_}')
-    argsort = np.argsort(cps[:, 1])
-    topLine, bottomLine = hLines[argsort[1]], hLines[argsort[-3]]
-    drawLine(img, topLine)
-    drawLine(img, bottomLine)
-    cv2.imshow("img", img)
-    # cv2.imshow("edges", edges)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    result = '|'.join(coordinates)
+    print(result)
 
 
 if __name__ == '__main__':
